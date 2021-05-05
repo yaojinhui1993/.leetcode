e4b421abeef0aa31b35efc735197b993bcca5c76
yaojinhui <yaojinhui1993@gmail.com>
2021-05-05 14:13:49 +0800

    35. seacrh insert position

---
+++ 35.搜索插入位置.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=35 lang=php
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (47.00%)
+ * Likes:    908
+ * Dislikes: 0
+ * Total Accepted:    371.2K
+ * Total Submissions: 789.7K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    public function searchInsert($nums, $target)
+    {
+        // 空数组
+        if (empty($nums)) {
+            return 0;
+        }
+
+        // 循环搜索
+        foreach ($nums as $key => $value) {
+            // 相等情况
+            if ($target === $value) {
+                return $key;
+            }
+
+            // 小于情况，往后找一个
+            if ($target < $value) {
+                $result = $key - 1;
+
+                if (isset($nums[$result])) {
+                    if ($target > $nums[$result]) {
+                        return $result + 1;
+                    } else {
+                        return $result;
+                    }
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        // 超出最大值了
+        return count($nums);
+    }
+}
+// @lc code=end
