6f97bbb7835c8455008889663b9d23847635b60e
yaojinhui <yaojinhui1993@gmail.com>
2021-05-11 12:58:48 +0800

    69. sqrtx

---
+++ 69.x-的平方根.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=69 lang=php
+ *
+ * [69] x 的平方根
+ *
+ * https://leetcode-cn.com/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (39.20%)
+ * Likes:    675
+ * Dislikes: 0
+ * Total Accepted:    306.1K
+ * Total Submissions: 780.3K
+ * Testcase Example:  '4'
+ *
+ * 实现 int sqrt(int x) 函数。
+ *
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ *
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ *
+ * 示例 1:
+ *
+ * 输入: 4
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842...,
+ * 由于返回类型是整数，小数部分将被舍去。
+ *
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+
+    // /**
+    //  * @param int $x
+    //  * @return int
+    //  */
+    // public function mySqrt($x)
+    // {
+    //     // system method
+    //     return (int)sqrt($x);
+    // }
+
+    /**
+     * @param int $x
+     * @return int
+     */
+    public function mySqrt($x)
+    {
+        if ($x === 2) {
+            return 1;
+        }
+
+        for ($i = 1; $i < $x; $i++) {
+            if ($i * $i === $x) {
+                return $i;
+            } elseif ($i * $i > $x) {
+                return $i - 1;
+            }
+        }
+
+        return $x;
+    }
+}
+// @lc code=end
