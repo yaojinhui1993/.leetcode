6f3721fb83dc52e591ddd2c13f8228c07b3564de
yaojinhui <yaojinhui1993@gmail.com>
2021-05-31 13:42:25 +0800

    168. excel-sheet-column-title
    failed at 701.Devided and remainder...
    
    I'm error...

---
+++ 168.excel表列名称.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=168 lang=php
+ *
+ * [168] Excel表列名称
+ *
+ * https://leetcode-cn.com/problems/excel-sheet-column-title/description/
+ *
+ * algorithms
+ * Easy (39.14%)
+ * Likes:    347
+ * Dislikes: 0
+ * Total Accepted:    51.2K
+ * Total Submissions: 130.3K
+ * Testcase Example:  '1'
+ *
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如，
+ *
+ * ⁠   1 -> A
+ * ⁠   2 -> B
+ * ⁠   3 -> C
+ * ⁠   ...
+ * ⁠   26 -> Z
+ * ⁠   27 -> AA
+ * ⁠   28 -> AB
+ * ⁠   ...
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "A"
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: 28
+ * 输出: "AB"
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: 701
+ * 输出: "ZY"
+ *
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+
+    /**
+     * @param int $columnNumber
+     * @return String
+     */
+    public function convertToTitle($columnNumber)
+    {
+        $str = '';
+
+        // 2147483647
+        while ((int)($columnNumber / 26)) {
+            if ($columnNumber / 26) {
+                $str = chr($columnNumber % 26 + 64) . $str;
+            } else {
+                $columnNumber = (int)($columnNumber / 26);
+            }
+        }
+
+        $str = chr($columnNumber + 64) . $str;
+
+        return $str;
+    }
+}
+// @lc code=end
