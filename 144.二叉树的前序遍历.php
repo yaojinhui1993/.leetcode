f83595c01d3469be3e0134b983432381fb93b57b
yaojinhui <yaojinhui1993@gmail.com>
2021-06-18 13:03:13 +0800

    144. binary-tree-preorder-traversal

---
+++ 144.二叉树的前序遍历.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=144 lang=php
+ *
+ * [144] 二叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (69.83%)
+ * Likes:    578
+ * Dislikes: 0
+ * Total Accepted:    336.6K
+ * Total Submissions: 482K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ *
+ *
+ * 示例 5：
+ *
+ *
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100
+ *
+ *
+ *
+ *
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution
+{
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    public function preorderTraversal($root)
+    {
+        // $orders = [];
+
+        $orders[] = $root->val;
+
+        if ($root->left) {
+            $orders = array_merge($orders, $this->preorderTraversal($root->left));
+        }
+
+
+        if ($root->right) {
+            $orders = array_merge($orders, $this->preorderTraversal($root->right));
+        }
+
+        return $orders;
+    }
+}
+// @lc code=end
